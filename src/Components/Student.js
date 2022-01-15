d33be9a51bf1dfeeddd990d8194006b7e94bdc7d
gymbundye <gymbundye@gmail.com>
2022-01-15 14:14:39 -0500

    started students hw

---
+++ src/Components/Student.js
@@ -0,0 +1,12 @@
+
+import React,{useState} from "react";
+function Student() {
+    const [studentData, setStudentData]= useState({})
+    return (
+      <div className="Student">
+      <h1>test</h1>
+      </div>
+    );
+  }
+  
+  export default Student;
